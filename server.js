0364eefe0c4d4187dea97f71149d39b6dacdd17b
Varto Yazılım Proje Yönetim <proje@vartoyazilim.com>
2026-05-04 02:56:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const http = require("http");
+const { Client } = require("pg");
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-  res.end("Coolify Dockerfile ile Node çalışıyor 🚀");
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: 5432,
+  user: "postgres",
+  password: "postgres",
+  database: "postgres",
+});
+
+client.connect()
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log("DB error:", err));
+
+const server = http.createServer(async (req, res) => {
+  try {
+    const result = await client.query("SELECT NOW()");
+    res.end("DB Time: " + result.rows[0].now);
+  } catch (err) {
+    res.end("DB ERROR: " + err.message);
+  }
 });
 
 server.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+  console.log("Server running");
 });
