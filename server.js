143513caf69b864e2409d54888d1bf70c499b22d
Varto Yazılım Proje Yönetim <proje@vartoyazilim.com>
2026-05-04 02:51:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Coolify Node çalışıyor 🚀");
+  res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+  res.end("Coolify Dockerfile ile Node çalışıyor 🚀");
 });
 
 server.listen(3000, "0.0.0.0", () => {
